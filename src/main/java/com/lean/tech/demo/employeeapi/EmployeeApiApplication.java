f7e006869cf961eb981572e7fe817b5d98b60513
Asgard R <asgard.rocha@kroger.com>
2021-07-21 10:18:06 -0500

    first commit

---
+++ src/main/java/com/lean/tech/demo/employeeapi/EmployeeApiApplication.java
@@ -0,0 +1,14 @@
+package com.lean.tech.demo.employeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		SpringApplication.run(EmployeeApiApplication.class, args);
+	}
+
+}
